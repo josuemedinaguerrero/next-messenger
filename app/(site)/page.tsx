73426430698b08a3c19e9544cb4cd7fe465ad0f0
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2023-09-19 14:49:24 -0500

    logo added

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image alt="Logo" height={48} width={48} src="/images/logo.png" className="mx-auto w-auto" />
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-800">Sign in to you account</h2>
+      </div>
+    </div>
+  );
+}
